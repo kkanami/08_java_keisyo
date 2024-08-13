3c097e6f63ff2cb398e5da4e82bebd2b4a4c7b97
kkanami <k3064782@gmail.com>
2024-08-13 22:06:41 +0900

    0813_2

---
+++ iphone/src/iphone/Android.java
@@ -0,0 +1,17 @@
+package iphone;
+
+public class Android {
+	public static void main(String[] args){
+		Smartphone android=new Smartphone();
+		android.play();
+		android.stop();
+		android.next();
+		android.back();
+		android.call();
+		android.mail();
+		android.photo();
+		android.internet();
+
+
+	}
+}
